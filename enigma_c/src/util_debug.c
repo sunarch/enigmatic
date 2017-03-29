dd6d60703a92c8449c059d0013782d4385cd0a20
András Németh <sunarchx@gmail.com>
2017-03-29 16:46:15 +0200

    Add column helper lines to debug format

--- enigma_c/src/util_debug.c
+++ enigma_c/src/util_debug.c
@@ -10,7 +10,7 @@ signed short debug_indent_level = 0;
 void print_debug_indent() {
     if (debug_indent_level > 0) {
         for (unsigned short n = 1; n <= debug_indent_level; ++n) {
-            printf("    ");
+            printf("|   ");
         }
     }
 }
