5d7c92c7ad4cd7de6a8eb79be9e374fbaa3cfb6b
András Németh <sunarchx@gmail.com>
2017-03-30 03:50:54 +0200

    Add wrapping macro to header file

--- enigma_c/src/util_debug.h
+++ enigma_c/src/util_debug.h
@@ -1,7 +1,13 @@
 
+#ifndef _UTIL_DEBUG_H_
+#define _UTIL_DEBUG_H_
+
 const unsigned short DEBUG;
 signed short debug_indent_level;
 
 void print_debug_indent(void);
 void inc_debug_indent(void);
 void dec_debug_indent(void);
+
+
+#endif
