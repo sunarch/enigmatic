4906b5158a93ffe76946da9c9a674ac0e9cb2f1d
András Németh <sunarch@protonmail.com>
2023-07-08 15:26:03 +0200

    Rename ASCII functions for consistency

--- src/view-ascii.c
+++ src/view-ascii.c
@@ -17,25 +17,25 @@
 
 // PRIVATE /////////////////////////////////////////////////////////////////////
 
-static void ascii_item_char_label(void)
+static void ascii_print_char_label(void)
 {
     printf("       ");
 }
 
 
-static void ascii_item_char_formatted(char character)
+static void ascii_print_char_formatted(char character)
 {
     printf("  '%c' ", character);
 }
 
 
-static void ascii_item_code_label(void)
+static void ascii_print_code_label(void)
 {
     printf("ASCII: ");
 }
 
 
-static void ascii_item_code_formatted(char character)
+static void ascii_print_code_formatted(char character)
 {
     printf(" ");
     debug_number_unsigned_hundred((unsigned short) character);
@@ -49,8 +49,8 @@ void ascii_print(char *p_text, unsigned short indent_length)
     pager_print(p_text,
                 indent_length,
                 PART_MAX_LENGTH,
-                &ascii_item_char_label,
-                &ascii_item_char_formatted,
-                &ascii_item_code_label,
-                &ascii_item_code_formatted);
+                &ascii_print_char_label,
+                &ascii_print_char_formatted,
+                &ascii_print_code_label,
+                &ascii_print_code_formatted);
 }
