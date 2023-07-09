1b1c7cd0be9a6addefbd6222da809abd38181e43
András Németh <sunarch@protonmail.com>
2023-07-09 15:31:46 +0200

    Compact morse code printing with lookup instead of if-branches

--- src/view-morse.c
+++ src/view-morse.c
@@ -6,6 +6,8 @@
 
 #include <stdio.h>
 
+#include "util-abc.h"
+#include "util-abc-common.h"
 #include "util-text.h"
 #include "view-morse.h"
 #include "view-pager.h"
@@ -13,42 +15,53 @@
 
 // MACROS //////////////////////////////////////////////////////////////////////
 
-#define  MORSE_CODE_MAX_LENGTH       6
-#define  MORSE_CODE_MAX_LENGTH_STR   7
-#define  PART_MAX_LENGTH            14
+#define  MORSE_CODE_MAX_LENGTH          6
+#define  MORSE_CODE_MAX_LENGTH_STRING   7
+#define  PART_MAX_LENGTH               14
 
 // CONSTANTS ///////////////////////////////////////////////////////////////////
 
-static const char A_MORSE [MORSE_CODE_MAX_LENGTH] = ".-";
-static const char B_MORSE [MORSE_CODE_MAX_LENGTH] = "-...";
-static const char C_MORSE [MORSE_CODE_MAX_LENGTH] = "-.-.";
-static const char D_MORSE [MORSE_CODE_MAX_LENGTH] = "-..";
-static const char E_MORSE [MORSE_CODE_MAX_LENGTH] = ".";
-static const char F_MORSE [MORSE_CODE_MAX_LENGTH] = "..-.";
-static const char G_MORSE [MORSE_CODE_MAX_LENGTH] = "--.";
-static const char H_MORSE [MORSE_CODE_MAX_LENGTH] = "....";
-static const char I_MORSE [MORSE_CODE_MAX_LENGTH] = "..";
-static const char J_MORSE [MORSE_CODE_MAX_LENGTH] = ".---";
-static const char K_MORSE [MORSE_CODE_MAX_LENGTH] = "-.-";
-static const char L_MORSE [MORSE_CODE_MAX_LENGTH] = ".-..";
-static const char M_MORSE [MORSE_CODE_MAX_LENGTH] = "--";
-static const char N_MORSE [MORSE_CODE_MAX_LENGTH] = "-.";
-static const char O_MORSE [MORSE_CODE_MAX_LENGTH] = "---";
-static const char P_MORSE [MORSE_CODE_MAX_LENGTH] = ".--.";
-static const char Q_MORSE [MORSE_CODE_MAX_LENGTH] = "--.-";
-static const char R_MORSE [MORSE_CODE_MAX_LENGTH] = ".-.";
-static const char S_MORSE [MORSE_CODE_MAX_LENGTH] = "...";
-static const char T_MORSE [MORSE_CODE_MAX_LENGTH] = "-";
-static const char U_MORSE [MORSE_CODE_MAX_LENGTH] = "..-";
-static const char V_MORSE [MORSE_CODE_MAX_LENGTH] = "...-";
-static const char W_MORSE [MORSE_CODE_MAX_LENGTH] = ".--";
-static const char X_MORSE [MORSE_CODE_MAX_LENGTH] = "-..-";
-static const char Y_MORSE [MORSE_CODE_MAX_LENGTH] = "-.--";
-static const char Z_MORSE [MORSE_CODE_MAX_LENGTH] = "--..";
+static const char A_MORSE [MORSE_CODE_MAX_LENGTH_STRING] = ".-";
+static const char B_MORSE [MORSE_CODE_MAX_LENGTH_STRING] = "-...";
+static const char C_MORSE [MORSE_CODE_MAX_LENGTH_STRING] = "-.-.";
+static const char D_MORSE [MORSE_CODE_MAX_LENGTH_STRING] = "-..";
+static const char E_MORSE [MORSE_CODE_MAX_LENGTH_STRING] = ".";
+static const char F_MORSE [MORSE_CODE_MAX_LENGTH_STRING] = "..-.";
+static const char G_MORSE [MORSE_CODE_MAX_LENGTH_STRING] = "--.";
+static const char H_MORSE [MORSE_CODE_MAX_LENGTH_STRING] = "....";
+static const char I_MORSE [MORSE_CODE_MAX_LENGTH_STRING] = "..";
+static const char J_MORSE [MORSE_CODE_MAX_LENGTH_STRING] = ".---";
+static const char K_MORSE [MORSE_CODE_MAX_LENGTH_STRING] = "-.-";
+static const char L_MORSE [MORSE_CODE_MAX_LENGTH_STRING] = ".-..";
+static const char M_MORSE [MORSE_CODE_MAX_LENGTH_STRING] = "--";
+static const char N_MORSE [MORSE_CODE_MAX_LENGTH_STRING] = "-.";
+static const char O_MORSE [MORSE_CODE_MAX_LENGTH_STRING] = "---";
+static const char P_MORSE [MORSE_CODE_MAX_LENGTH_STRING] = ".--.";
+static const char Q_MORSE [MORSE_CODE_MAX_LENGTH_STRING] = "--.-";
+static const char R_MORSE [MORSE_CODE_MAX_LENGTH_STRING] = ".-.";
+static const char S_MORSE [MORSE_CODE_MAX_LENGTH_STRING] = "...";
+static const char T_MORSE [MORSE_CODE_MAX_LENGTH_STRING] = "-";
+static const char U_MORSE [MORSE_CODE_MAX_LENGTH_STRING] = "..-";
+static const char V_MORSE [MORSE_CODE_MAX_LENGTH_STRING] = "...-";
+static const char W_MORSE [MORSE_CODE_MAX_LENGTH_STRING] = ".--";
+static const char X_MORSE [MORSE_CODE_MAX_LENGTH_STRING] = "-..-";
+static const char Y_MORSE [MORSE_CODE_MAX_LENGTH_STRING] = "-.--";
+static const char Z_MORSE [MORSE_CODE_MAX_LENGTH_STRING] = "--..";
 
 // non-letters
 
-static const char DOT_MORSE [MORSE_CODE_MAX_LENGTH] = ".-.-.-";
+static const char DOT_MORSE [MORSE_CODE_MAX_LENGTH_STRING] = ".-.-.-";
+
+// reference list
+
+static const char *MORSE_LETTERS [ABC_LENGTH_STRING] = {
+        A_MORSE, B_MORSE, C_MORSE, D_MORSE, E_MORSE,
+        F_MORSE, G_MORSE, H_MORSE, I_MORSE, J_MORSE,
+        K_MORSE, L_MORSE, M_MORSE, N_MORSE, O_MORSE,
+        P_MORSE, Q_MORSE, R_MORSE, S_MORSE, T_MORSE,
+        U_MORSE, V_MORSE, W_MORSE, X_MORSE, Y_MORSE,
+        Z_MORSE
+};
 
 
 // PRIVATE /////////////////////////////////////////////////////////////////////
@@ -62,91 +75,17 @@ static void morse_print_char_padded(const char *code_text)
 
 static void morse_print_char(char character)
 {
-        switch (character) {
-                case 'a':
-                        morse_print_char_padded(A_MORSE);
-                        break;
-                case 'b':
-                        morse_print_char_padded(B_MORSE);
-                        break;
-                case 'c':
-                        morse_print_char_padded(C_MORSE);
-                        break;
-                case 'd':
-                        morse_print_char_padded(D_MORSE);
-                        break;
-                case 'e':
-                        morse_print_char_padded(E_MORSE);
-                        break;
-                case 'f':
-                        morse_print_char_padded(F_MORSE);
-                        break;
-                case 'g':
-                        morse_print_char_padded(G_MORSE);
-                        break;
-                case 'h':
-                        morse_print_char_padded(H_MORSE);
-                        break;
-                case 'i':
-                        morse_print_char_padded(I_MORSE);
-                        break;
-                case 'j':
-                        morse_print_char_padded(J_MORSE);
-                        break;
-                case 'k':
-                        morse_print_char_padded(K_MORSE);
-                        break;
-                case 'l':
-                        morse_print_char_padded(L_MORSE);
-                        break;
-                case 'm':
-                        morse_print_char_padded(M_MORSE);
-                        break;
-                case 'n':
-                        morse_print_char_padded(N_MORSE);
-                        break;
-                case 'o':
-                        morse_print_char_padded(O_MORSE);
-                        break;
-                case 'p':
-                        morse_print_char_padded(P_MORSE);
-                        break;
-                case 'q':
-                        morse_print_char_padded(Q_MORSE);
-                        break;
-                case 'r':
-                        morse_print_char_padded(R_MORSE);
-                        break;
-                case 's':
-                        morse_print_char_padded(S_MORSE);
-                        break;
-                case 't':
-                        morse_print_char_padded(T_MORSE);
-                        break;
-                case 'u':
-                        morse_print_char_padded(U_MORSE);
-                        break;
-                case 'v':
-                        morse_print_char_padded(V_MORSE);
-                        break;
-                case 'w':
-                        morse_print_char_padded(W_MORSE);
-                        break;
-                case 'x':
-                        morse_print_char_padded(X_MORSE);
-                        break;
-                case 'y':
-                        morse_print_char_padded(Y_MORSE);
-                        break;
-                case 'z':
-                        morse_print_char_padded(Z_MORSE);
-                        break;
-                case '.':
+        signed short letter_index = abc_index_from_char_lower(character);
+        if (letter_index < 0) {
+                if (character == '.') {
                         morse_print_char_padded(DOT_MORSE);
-                        break;
-                default:
+                }
+                else {
                         morse_print_char_padded("");
-                        break;
+                }
+        }
+        else {
+                morse_print_char_padded(MORSE_LETTERS[letter_index]);
         }
         printf(" ");
 }
